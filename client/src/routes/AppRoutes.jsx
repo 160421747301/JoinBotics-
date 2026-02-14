aff495368b77ac7a0805c3f3c4177684a2a1bc28
Zaka <mohdzakakareem@gmail.com>
2026-02-14 17:32:40 +0530

    Add Certifications route to application routing

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Certifications from "../pages/Certifications";
 import GlobalExposure from "../pages/GlobalExposure";
 import Programs from "../pages/Programs";
 import Products from "../pages/Products";
@@ -13,12 +14,15 @@ import Careers from "../pages/Careers";
 import FAQ from "../pages/FAQ";
 import AIProjects from "../pages/AIProjects";
 import AdminEmails from "../pages/AdminEmails";
+import PrivacyPolicy from "../pages/PrivacyPolicy";
+import TermsOfService from "../pages/TermsOfService";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/certifications" element={<Certifications />} />
       <Route path="/global-exposure" element={<GlobalExposure />} />
       <Route path="/programs" element={<Programs />} />
       <Route path="/products" element={<Products />} />
@@ -30,6 +34,8 @@ export default function AppRoutes() {
       <Route path="/faq" element={<FAQ />} />
       <Route path="/ai-projects" element={<AIProjects />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms-of-service" element={<TermsOfService />} />
       <Route path="/admin/manage-emails" element={<AdminEmails />} />
       <Route path="*" element={<Home />} />
     </Routes>
